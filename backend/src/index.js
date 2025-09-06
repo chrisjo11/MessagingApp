80285d2d22df3206ec05318138f7a34c53485b97
Christopher Jo <chrisjo1105@gmail.com>
2025-09-06 18:43:10 -0400

    confirmation of proper database connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,5 @@
 import express from "express"
-import dotenv from ".env"
+import dotenv from "dotenv"
 
 import authRoutes from "./routes/auth.routes.js"
 import { connectDB } from "./lib/db.js"
@@ -13,5 +13,5 @@ app.use("/api/auth", authRoutes)
 
 app.listen(PORT, () => {
     console.log("Server is running on PORT: " + PORT)
-    connectDB
+    connectDB()
 })
