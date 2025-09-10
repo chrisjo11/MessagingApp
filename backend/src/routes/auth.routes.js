668e2c819d9672a8533b72dbdbf70ef94d1052ad
Christopher Jo <chrisjo1105@gmail.com>
2025-09-09 23:13:28 -0400

    updated routes file

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,6 +1,6 @@
 import express from "express"
-
-import { signup, login, logout } from "../controllers/auth.controller.js"
+import { protectRoute } from "../middleware/auth.middleware.js"
+import { signup, login, logout, updateProfile } from "../controllers/auth.controller.js"
 
 const router = express.Router()
 
